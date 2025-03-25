ffdccce5e06f5ab17e57cbedb8933550088fb8a1
mvas-procont <mihail.vasiliev@procont.ru>
2025-03-25 23:45:04 +0300

    изменения в db

--- db.js
+++ db.js
@@ -3,8 +3,8 @@ const { Sequelize } = require('sequelize');
 // Создаем экземпляр Sequelize, передавая параметры подключения к базе данных
 const sequelize = new Sequelize({
   database: 'kiks_app', // Название базы данных
-  username: 'root', // Имя пользователя базы данных
-  password: 'root', // Пароль пользователя базы данных
+  username: 'kiks_user', // Имя пользователя базы данных
+  password: 'kiks_password', // Пароль пользователя базы данных
   host: 'localhost', // Хост базы данных (например, localhost)
   port: 5432, // Порт базы данных (по умолчанию для PostgreSQL 5432)
   dialect: 'postgres', // Диалект базы данных (postgres, mysql, sqlite, mssql)
