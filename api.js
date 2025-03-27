5b4fd26d67ffdbaf83488ce30773a6060815706b
mvas-procont <mihail.vasiliev@procont.ru>
2025-03-27 18:14:46 +0300

    исправление api на нужные параметры

--- api.js
+++ api.js
@@ -25,10 +25,30 @@ router.get('/bookings', async (req, res) => {
   }
 });
 
-// Получить пользователя
-router.get('/get-user:chat_id', async (req, res) => {
+// // Получить пользователя
+// router.get('/get-user:chat_id', async (req, res) => {
+//   try {
+//     const user = await User.findOne({ where: { chat_id: chat_id } });
+//     res.json(user);
+//   } catch (err) {
+//     res.status(500).json({ error: err.message });
+//   }
+// });
+
+router.get('/get-user', async (req, res) => {
   try {
+    const { chat_id } = req.query; // Получаем chatId из query-параметров (?chatId=123)
+    
+    if (!chat_id) {
+      return res.status(400).json({ error: 'chatId is required' });
+    }
+    
     const user = await User.findOne({ where: { chat_id: chat_id } });
+    
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    
     res.json(user);
   } catch (err) {
     res.status(500).json({ error: err.message });
