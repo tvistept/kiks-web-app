a7eda938e7e625c7f6c6a257d6ee95f3135a63cf
mvas-procont <mihail.vasiliev@procont.ru>
2025-10-28 00:31:04 +0300

    интеграция гугла

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const TelegramBot = require('node-telegram-bot-api');
+const { google } = require('googleapis');
 const apiRouter = require('./api');
 const { tg_token, google_worksheet_id, google_sheet_id, google_worksheet_id_kiks2, tg_token_kiks2 } = require('/app-configs/tokens.js');
-// const tg_token = '7579297753:AAGygIX_wPxh2VcJaWe3PSpz12ri3jrCFwM';
+const USER1_SHEET_ID = google_worksheet_id_kiks2;
+const USER2_SHEET_ID = google_worksheet_id_kiks2;
+const SERVICE_SHEET_ID = google_worksheet_id;
 const WEB_APP_URL = 'https://tvistept.github.io/kiks-test-react-app/';
+const KEY_FILE = '/app-configs/google.json';
 const bot = new TelegramBot(tg_token_kiks2, { polling: true });
 const sequelize = require('./db');
 const { Op } = require('sequelize');
@@ -56,8 +60,203 @@ async function testConnection() {
         console.error('Unable to connect to the database:', error);
     }
 }
-
 testConnection();
+
+//единый клиент для всех запросов
+const auth = new google.auth.GoogleAuth({
+  keyFile: KEY_FILE,
+  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+});
+
+function dateFromString(dateString) {
+  const [day, month, year] = dateString.split('.').map(Number);
+  const date = new Date(year, month - 1, day);
+  return date;
+}
+
+function isWeekend(date) {
+    const day = date.getDay(); // 0 - воскресенье, 1 - понедельник, ..., 6 - суббота
+    return day === 0 || day === 5 || day === 6; // 0 (воскресенье), 5 (пятница), 6 (суббота)
+}
+
+async function appendRow(spreadsheetId, range, values) {
+  const resource = { values };
+  const response = await sheetsClient.spreadsheets.values.append({
+    spreadsheetId,
+    range,
+    valueInputOption: 'RAW', // Можно использовать RAW или USER_ENTERED
+    resource,
+  });
+
+  console.log(`${response.data.updates.updatedCells} ячеек добавлено.`);
+}
+
+async function writeToCell(spreadsheetId, range, value) {
+  const values = [ [value] ];
+  const resource = { values };
+  const response = await sheetsClient.spreadsheets.values.update({
+    spreadsheetId,
+    range,
+    valueInputOption: 'USER_ENTERED', // или 'RAW'
+    resource,
+  });
+
+  console.log(`${response.data.updatedCells} ячеек обновлено.`);
+}
+
+async function mergeCells(sheets, range, spreadsheetId) {
+  const { sheetName, cellIndex1, cellIndex2, row } = getRangeObject(range);
+//   const spreadsheetId = USER_SHEET_ID;
+
+  // Получаем sheetId по имени листа
+  const sheetRes = await sheets.spreadsheets.get({ spreadsheetId });
+  const sheet = sheetRes.data.sheets.find(s => s.properties.title === sheetName);
+  const sheetIdToMerge = sheet.properties.sheetId;
+
+  // Формируем запрос на объединение A2:B2 (строка 2 = индекс 1, столбцы A=0, B=1)
+  const request = {
+    spreadsheetId,
+    resource: {
+      requests: [
+        {
+          mergeCells: {
+            range: {
+              sheetId:sheetIdToMerge,
+              startRowIndex: row-1,    // строка 2 (индексация с 0)
+              endRowIndex: row,      // до строки 3 (не включая)
+              startColumnIndex: cellIndex1, // столбец A (индексация с 0)
+              endColumnIndex: cellIndex2+1    // до столбца C (не включая)
+            },
+            mergeType: 'MERGE_ALL'
+          }
+        }
+      ]
+    }
+  };
+
+  // Выполняем запрос
+  await sheets.spreadsheets.batchUpdate(request);
+  console.log(`Ячейки успешно объединены на листе ${sheetName}.`);
+}
+
+async function writeToRange(spreadsheetId, range, value, unmerge = false) {
+  const values = [ value ];
+  const resource = { values };
+  const response = await sheetsClient.spreadsheets.values.update({
+    spreadsheetId,
+    range,
+    valueInputOption: 'USER_ENTERED', // или 'RAW'
+    resource,
+  });
+
+  console.log(`${response.data.updatedCells} ячеек обновлено.`);
+  if (unmerge) {
+    unmergeCells(sheetsClient, range, spreadsheetId);
+  } else {
+    mergeCells(sheetsClient, range, spreadsheetId);
+  }
+}
+
+async function getSheetLink(sheetName, spreadsheetId) {
+//   const spreadsheetId = USER_SHEET_ID;
+  // Получаем список листов и их свойства
+  const res = await sheetsClient.spreadsheets.get({ spreadsheetId });
+  const sheet = res.data.sheets.find(s => s.properties.title === sheetName);
+
+  if (!sheet) {
+    throw new Error(`Лист "${sheetName}" не найден.`);
+  }
+
+  const gid = sheet.properties.sheetId;
+  const link = `https://docs.google.com/spreadsheets/d/${spreadsheetId}/edit#gid=${gid}`;
+  return link;
+}
+
+async function unmergeCells(sheets, range, spreadsheetId) {
+  const { sheetName, cellIndex1, cellIndex2, row } = getRangeObject(range);
+  
+
+  // Получаем sheetId по имени листа
+  const sheetRes = await sheets.spreadsheets.get({ spreadsheetId });
+  const sheet = sheetRes.data.sheets.find(s => s.properties.title === sheetName);
+  const sheetIdToUnmerge = sheet.properties.sheetId;
+
+  // Формируем запрос на объединение A2:B2 (строка 2 = индекс 1, столбцы A=0, B=1)
+  const request = {
+    spreadsheetId,
+    resource: {
+      requests: [
+        {
+          unmergeCells: {
+            range: {
+              sheetId:sheetIdToUnmerge,
+              startRowIndex: row-1,    // строка 2 (индексация с 0)
+              endRowIndex: row,      // до строки 3 (не включая)
+              startColumnIndex: cellIndex1, // столбец A (индексация с 0)
+              endColumnIndex: cellIndex2+1    // до столбца C (не включая)
+            },
+          }
+        }
+      ]
+    }
+  };
+
+  // Выполняем запрос
+  await sheets.spreadsheets.batchUpdate(request);
+  console.log(`Ячейки успешно разъединены на листе ${sheetName}.`);
+}
+
+async function bookTable(bookDate, bookTime, tableNum, hours, userName, club) { 
+    try {
+      // Определяем колонку для времени
+      let timeToColumn = {}
+      isWeekend(dateFromString(bookDate)) ? timeToColumn = {
+          '12:00': 'C',
+          '13:00': 'D',
+          '14:00': 'E',
+          '15:00': 'F',
+          '16:00': 'G',
+          '17:00': 'H',
+          '18:00': 'I',
+          '19:00': 'J',
+          '20:00': 'K',
+          '21:00': 'L',
+          '22:00': 'M',
+          '23:00': 'N',
+          '00:00': 'O',
+          '01:00': 'P',
+      } : timeToColumn = {
+          '14:00': 'C',
+          '15:00': 'D',
+          '16:00': 'E',
+          '17:00': 'F',
+          '18:00': 'G',
+          '19:00': 'H',
+          '20:00': 'I',
+          '21:00': 'J',
+          '22:00': 'K',
+          '23:00': 'L',
+          '00:00': 'M',
+          '01:00': 'N'
+      };
+
+      let sheet_id = club === 'kiks2' ? USER2_SHEET_ID : USER1_SHEET_ID;
+      const startColumn = timeToColumn[bookTime];
+      const startRow = parseInt(tableNum) + 1; // Строка = номер стола + 1
+
+      if (parseInt(hours) === 2) {
+          const nextColumn = String.fromCharCode(startColumn.charCodeAt(0) + 1);
+          await writeToRange(sheet_id, `${bookDate}!${startColumn}${startRow}:${nextColumn}${startRow}`, [userName, userName]);
+      } else {
+        await writeToCell(sheet_id, `${bookDate}!${startColumn}${startRow}`, userName);
+      }
+      return true
+    } catch (error) {
+      console.log(error)
+      return false
+    }
+    
+}
 
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
@@ -132,6 +331,7 @@ bot.on('message', async (msg) => {
             const dateString = data.date;
             const [year, month, day] = dateString.split('-');
             const formattedDate = `${day}.${month}.${year}`;
+            let clubId = data.club === 'Каменноостровский 26-28' ? 'kiks2' : 'kiks1';
 
             let infoMessage = `\nОбщая информация:\n• ${data.club}\n• ${formattedDate}\n• ${data.time}\n• стол №${data.table}\n• ${data.hours} ${prefix}`
             let infoMessage1 = `Внутри мы сделали веджи-кухню и пивной крафтовый бар. Просим, не приносить свою еду и напитки.`
@@ -147,6 +347,7 @@ bot.on('message', async (msg) => {
                 }
             )
             await bot.sendMessage(chatId, finalMessage, {parse_mode: 'HTML', no_webpage:true, disable_web_page_preview:true, link_preview_options: {is_disabled: true}});
+            await bookTable(data.date, data.time, data.table, data.hours, data.name, clubId);
 
         } catch (error) {
             console.error(error);
