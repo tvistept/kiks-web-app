1171728663814746a9175f665aa2be6479d8974a
mvas-procont <mihail.vasiliev@procont.ru>
2026-08-20 15:44:04 +0300

    особый график для севкабеля 3

--- index.js
+++ index.js
@@ -385,8 +385,8 @@ async function bookTable(bookDate, bookTime, tableNum, hours, userName, club) {
 
     if (club === 'kiks3' && checkDate >= startDate) {
       isWeekend(dateFromString(bookDate), club) ? 
-      timeToColumn = { '14:00': 'C', '15:00': 'D', '16:00': 'E', '17:00': 'F', '18:00': 'G', '19:00': 'H', '20:00': 'I', '21:00': 'J', '22:00': 'K'} :
-      timeToColumn = { '12:00': 'C', '13:00': 'D', '14:00': 'E', '15:00': 'F', '16:00': 'G', '17:00': 'H', '18:00': 'I', '19:00': 'J', '20:00': 'K', '21:00': 'L', '22:00': 'M', '23:00': 'N', '00:00': 'O', '01:00': 'P', }
+      timeToColumn = { '12:00': 'C', '13:00': 'D', '14:00': 'E', '15:00': 'F', '16:00': 'G', '17:00': 'H', '18:00': 'I', '19:00': 'J', '20:00': 'K', '21:00': 'L', '22:00': 'M', '23:00': 'N', '00:00': 'O', '01:00': 'P', } :
+      timeToColumn = { '14:00': 'C', '15:00': 'D', '16:00': 'E', '17:00': 'F', '18:00': 'G', '19:00': 'H', '20:00': 'I', '21:00': 'J', '22:00': 'K'}
     }
 
     let sheet_id = getSheetId(club);
@@ -452,8 +452,8 @@ async function deleteBooking(bookDate, bookTime, tableNum, hours, clubId) {
 
       if (clubId === 'kiks3' && checkDate >= startDate) {
         isWeekend(dateFromString(bookDate), clubId) ? 
-        timeToColumn = { '14:00': 'C', '15:00': 'D', '16:00': 'E', '17:00': 'F', '18:00': 'G', '19:00': 'H', '20:00': 'I', '21:00': 'J', '22:00': 'K'} :
-        timeToColumn = { '12:00': 'C', '13:00': 'D', '14:00': 'E', '15:00': 'F', '16:00': 'G', '17:00': 'H', '18:00': 'I', '19:00': 'J', '20:00': 'K', '21:00': 'L', '22:00': 'M', '23:00': 'N', '00:00': 'O', '01:00': 'P', }
+        timeToColumn = { '12:00': 'C', '13:00': 'D', '14:00': 'E', '15:00': 'F', '16:00': 'G', '17:00': 'H', '18:00': 'I', '19:00': 'J', '20:00': 'K', '21:00': 'L', '22:00': 'M', '23:00': 'N', '00:00': 'O', '01:00': 'P', } :
+        timeToColumn = { '14:00': 'C', '15:00': 'D', '16:00': 'E', '17:00': 'F', '18:00': 'G', '19:00': 'H', '20:00': 'I', '21:00': 'J', '22:00': 'K'} 
       }
 
       let spreadsheetId = getSheetId(clubId);
